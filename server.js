f313cdcb64cd024e7203dfaf51916d9ed9c991da
singleshade <54930671+singleshade@users.noreply.github.com>
2020-09-25 19:21:27 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.set('view engine', 'ejs');
+app.set('views', 'views');// it by default goes to views folder if we dont specify this line
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+});
+
+const server = app.listen(9000, () =>{
+  console.log("listening on port %s-", server.address().port);
+})
