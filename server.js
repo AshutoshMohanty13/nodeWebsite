28d8b8ae831049d447049464444b431b0d18cfe3
singleshade <54930671+singleshade@users.noreply.github.com>
2020-10-07 11:54:57 +0530

    SQL

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const db = require('./util/database');
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -14,6 +15,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+
+// db.execute('SELECT * FROM products')
+//   .then(result => {
+//     console.log(result[0]);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });//gets executed in case of any error
 
 app.use((req, res, next) => {
   res.status(404).render('404', { pageTitle: 'Page Not Found' });
