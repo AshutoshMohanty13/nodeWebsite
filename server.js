2735ddfb26464cd403051017f0e86a0987f0837b
singleshade <54930671+singleshade@users.noreply.github.com>
2020-10-13 18:33:06 +0530

    sequalize finished

--- server.js
+++ server.js
@@ -59,6 +59,81 @@
 //     console.log(err);
 //   });
 
+// const path = require('path');
+
+// const express = require('express');
+// const bodyParser = require('body-parser');
+
+// const errorController = require('./controllers/error');
+// const sequelize = require('./util/database');
+// const Product = require('./models/product');
+// const User = require('./models/user');
+// const Cart = require('./models/Cart');
+// const CartItem = require('./models/Cart-item');
+
+
+
+// const app = express();
+
+// app.set('view engine', 'ejs');
+// app.set('views', 'views');
+
+// const adminRoutes = require('./routes/admin');
+// const shopRoutes = require('./routes/shop');
+
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use((req, res, next) => {
+//   User.findByPk(1)
+//     .then(user => {
+//       req.user = user;
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
+
+// app.use('/admin', adminRoutes);
+// app.use(shopRoutes);
+
+// app.use(errorController.get404);
+
+// Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+// User.hasMany(Product);
+// User.hasOne(Cart);
+// Cart.belongsTo(User); // optional and its inverse of the User.hasOne(Cart);
+// Cart.belongsToMany(Product, { through : CartItem});// through tell the model wher these connections should be stored
+// Product.belongsToMany(Cart, { through : CartItem});
+
+// sequelize
+//    //.sync({ force: true })
+//   .sync()
+//   .then(result => {
+//     return User.findByPk(1);
+//     // console.log(result);
+//   })
+//   .then(user => {
+//     if (!user) {
+//       return User.create({ name: 'Ashutosh', email: 'test@test.com' });
+//     }
+//     return user;
+//   })
+//   .then(user => {
+//     //console.log(user);
+//     return User.createCart();    
+//   })
+//   .then(cart => {
+//     const server = app.listen(9000, () =>{
+//       console.log("listening on port %s-", server.address().port);
+//     });
+//   }
+    
+//   )
+//   .catch(err => {
+//     console.log(err);
+//   });
+
+
 const path = require('path');
 
 const express = require('express');
@@ -68,6 +143,10 @@ const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 const app = express();
 
@@ -96,6 +175,13 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
 
 sequelize
   // .sync({ force: true })
@@ -111,10 +197,13 @@ sequelize
     return user;
   })
   .then(user => {
-    //console.log(user);
+    // console.log(user);
+    return user.createCart();
+  })
+  .then(cart => {
     const server = app.listen(9000, () =>{
-          console.log("listening on port %s-", server.address().port);
-        });
+      console.log("listening on port %s-", server.address().port);
+          });
     
   })
   .catch(err => {
