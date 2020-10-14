5d9d9cde5b7d3404d70fb88706c052633b8fdca1
singleshade <54930671+singleshade@users.noreply.github.com>
2020-10-14 16:55:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -59,80 +59,6 @@
 //     console.log(err);
 //   });
 
-// const path = require('path');
-
-// const express = require('express');
-// const bodyParser = require('body-parser');
-
-// const errorController = require('./controllers/error');
-// const sequelize = require('./util/database');
-// const Product = require('./models/product');
-// const User = require('./models/user');
-// const Cart = require('./models/Cart');
-// const CartItem = require('./models/Cart-item');
-
-
-
-// const app = express();
-
-// app.set('view engine', 'ejs');
-// app.set('views', 'views');
-
-// const adminRoutes = require('./routes/admin');
-// const shopRoutes = require('./routes/shop');
-
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use((req, res, next) => {
-//   User.findByPk(1)
-//     .then(user => {
-//       req.user = user;
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
-
-// app.use('/admin', adminRoutes);
-// app.use(shopRoutes);
-
-// app.use(errorController.get404);
-
-// Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
-// User.hasMany(Product);
-// User.hasOne(Cart);
-// Cart.belongsTo(User); // optional and its inverse of the User.hasOne(Cart);
-// Cart.belongsToMany(Product, { through : CartItem});// through tell the model wher these connections should be stored
-// Product.belongsToMany(Cart, { through : CartItem});
-
-// sequelize
-//    //.sync({ force: true })
-//   .sync()
-//   .then(result => {
-//     return User.findByPk(1);
-//     // console.log(result);
-//   })
-//   .then(user => {
-//     if (!user) {
-//       return User.create({ name: 'Ashutosh', email: 'test@test.com' });
-//     }
-//     return user;
-//   })
-//   .then(user => {
-//     //console.log(user);
-//     return User.createCart();    
-//   })
-//   .then(cart => {
-//     const server = app.listen(9000, () =>{
-//       console.log("listening on port %s-", server.address().port);
-//     });
-//   }
-    
-//   )
-//   .catch(err => {
-//     console.log(err);
-//   });
-
 
 const path = require('path');
 
@@ -151,7 +77,7 @@ const OrderItem = require('./models/order-item');
 const app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set('views', 'views');// it by default goes to views folder if we dont specify this line
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -173,16 +99,19 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+//establishing connections or relations
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
 User.hasOne(Cart);
 Cart.belongsTo(User);
-Cart.belongsToMany(Product, { through: CartItem });
+Cart.belongsToMany(Product, { through: CartItem });// through tell the in between model where these connections should be stored
 Product.belongsToMany(Cart, { through: CartItem });
 Order.belongsTo(User);
 User.hasMany(Order);
 Order.belongsToMany(Product, { through: OrderItem });
 
+//it syncs you model to the database by creating the tables if its not already there, and if its already
+//present then it doesnt overwrite it.
 sequelize
   // .sync({ force: true })
   .sync()
